52c98793d1e0679ad33d4bfd973306de0f2a34bb
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-03-19 12:55:36 -0300

    Application config developed

--- src/main/java/com/digitalinnovationone/heroesapi/HeroesapiApplication.java
+++ src/main/java/com/digitalinnovationone/heroesapi/HeroesapiApplication.java
@@ -1,13 +1,19 @@
 package com.digitalinnovationone.heroesapi;
 
+import lombok.extern.slf4j.Slf4j;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@EnableDynamoDBRepositories
 public class HeroesapiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HeroesapiApplication.class, args);
+
 	}
 
 }
